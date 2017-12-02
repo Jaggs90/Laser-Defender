b513bd75f2867ca7c1a51d51b75433caa7d0ae5b
Dan Jaggs <djaggs@gmail.com>
2017-12-02 11:27:19 +0000

    no message

---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 15.0f;
+	public float padding =1f;
+	
+	float xMin = -5;
+	float xMax = 5;
+	
+	void Start() {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		xMin = leftmost.x + padding;
+		xMax = rightmost.x - padding;
+	}
+	
+
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			
+			transform.position += Vector3.left * speed* Time.deltaTime;
+			}else if (Input.GetKey(KeyCode.RightArrow)){
+			transform.position += Vector3.right * speed* Time.deltaTime;	
+	}
+		//Restrict the player to the game space
+		float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+ }
+}
